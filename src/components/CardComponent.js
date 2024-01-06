0c0957e341c94c823f7e0cae3967829696035c9e
Bakk <ion-menghini@hotmail.it>
2024-01-06 22:10:56 +0100

    Changes: -Projects page now load cards info with my repos from github

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -4,23 +4,24 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import { useHref } from 'react-router-dom';
 
-export default function ActionAreaCard() {
+export default function ActionAreaCard({ nome, ultimaModifica, link_to }) {
     return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardActionArea>
+        <Card sx={{ maxWidth: 345 }} >
+            <CardActionArea href={link_to} target="_blank">
                 <CardMedia
                     component="img"
-                    height="90"
+                    height="60"
                     image='src\img\bakk-logo.png'
                     alt="bakk logo"
                 />
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
-                        Bakk
+                        {nome.slice(0, 20)}
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                        Bakks ....
+                        {ultimaModifica}
                     </Typography>
                 </CardContent>
             </CardActionArea>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,3 +1,7 @@
+/// DATA ///
+import { Octokit } from "octokit";
+import git_file from '../github_token.json';
+
 /// STYLE ///
 import '../my-css/fonts/fonts.css';
 import '../my-css/projects.css';
@@ -7,7 +11,31 @@ import CardComponent from '../components/CardComponent';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-/// COMPONENTS ///
+
+// Octokit.js
+// https://github.com/octokit/core.js#readme
+
+// Specify the path to the text file
+const insert_token_github = git_file.token.toString();
+
+const octokit = new Octokit({
+    auth: insert_token_github
+})
+
+const github_request = await octokit.request('GET /user/repos', {
+    headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+    }
+})
+
+for (let index = 0; index < github_request.data.length; index++) {
+    const element = github_request.data[index];
+    console.log(element.full_name);
+    console.log(element.updated_at);
+    console.log(element.url);
+}
+
+
 
 export const Projects = () => {
     const boxStyle = {
@@ -35,12 +63,11 @@ export const Projects = () => {
                     </Row>
                     <Row>
                         <Row xs={1} md={3} className="g-4">
-                            {Array.from({ length: 9 }).map((_, idx) => (
+                            {Array.from({ length: github_request.data.length }).map((_, idx) => (
                                 <Col key={idx}>
-                                    <CardComponent />
+                                    <CardComponent nome={github_request.data[idx].full_name} ultimaModifica={github_request.data[idx].updated_at} link_to={github_request.data[idx].html_url} />
                                 </Col>
                             ))}
-                            <a>https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#get-all-repository-topics </a>
                         </Row>
                     </Row>
                 </Col>
@@ -50,7 +77,7 @@ export const Projects = () => {
                         Here i can put some cards with the coding languages i know
                     </p>
                 </Col>
-            </Row>
-        </Container>
+            </Row >
+        </Container >
     );
 }
